01db7bc446dd125e228dd4ff8d9f258492e1004b
Berkant <telliberkant@gmail.com>
2023-06-18 01:26:26 +0300

    lost in space

--- src/main/java/com/dir_music/bff/web/controller/auth_controller/AuthenticationController.java
+++ src/main/java/com/dir_music/bff/web/controller/auth_controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController("AuthenticationController")
 @RequestMapping("/")
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     final AuthenticationControllerFeign authenticationControllerFeign;
